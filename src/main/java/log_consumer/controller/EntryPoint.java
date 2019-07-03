246d1b13c99a0cb0df6d179947c363fdd4aa57a5
Anthony Tran <antinywong@gmail.com>
2019-07-03 12:04:12 -0700

    Updated Kafka Consumer call with option of local or Aiven Kafka with SSL.

--- src/main/java/log_consumer/controller/EntryPoint.java
+++ src/main/java/log_consumer/controller/EntryPoint.java
@@ -6,8 +6,14 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 
 public class EntryPoint extends AbstractVerticle {
@@ -20,20 +26,16 @@ public class EntryPoint extends AbstractVerticle {
 
         router.get("/api/log").handler(LogController::getLog);
 
+        // Uncomment out this section and comment out next section if you are using LOCAL KAFKA SERVER WITHOUT SSL.
+        /*
         Map<String, String> config = new HashMap<>();
         config.put("bootstrap.servers", kafkaServer);
-        config.put("security.protocol", "SSL");
-        config.put("ssl.truststore.location", "C:\\Users\\trana\\Desktop\\keys\\client.truststore.jks");
-        config.put("ssl.truststore.password", "password");
-        config.put("ssl.keystore.type", "PKCS12");
-        config.put("ssl.keystore.location", "C:\\Users\\trana\\Desktop\\keys\\client.keystore.p12");
-        config.put("ssl.keystore.password", "password");
-        config.put("ssl.key.password", "password");
         config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         config.put("value.deserializer", "io.vertx.kafka.client.serialization.JsonObjectDeserializer");
         config.put("group.id", "my_group");
         config.put("auto.offset.reset", "latest");
         config.put("enable.auto.commit", "false");
+
 
         // Use consumer for interacting with Apache Kafka
         KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, config);
@@ -49,6 +51,54 @@ public class EntryPoint extends AbstractVerticle {
             else
                 System.out.println("Failed Subscribe.");
         });
+
+        */
+
+        //Uncomment this section out and comment out above section if using AIVEN KAFKA SERVER.
+        try (InputStream input = new FileInputStream(new File("config.properties").getAbsolutePath())) {
+            //Load props file content.
+            Properties prop = new Properties();
+            prop.load(input);
+
+            //Extract props file content
+            String trustStorePath = new File(prop.getProperty("kafka.truststore.path")).getAbsolutePath();
+            String keyStorePath = new File(prop.getProperty("kafka.keystore.path")).getAbsolutePath();
+            String trustStorePassword = prop.getProperty("kafka.truststore.password");
+            String keyStorePassword = prop.getProperty("kafka.keystore.password");
+
+            Map<String, String> config = new HashMap<>();
+            config.put("bootstrap.servers", kafkaServer);
+            config.put("security.protocol", "SSL");
+            config.put("ssl.truststore.location", trustStorePath);
+            config.put("ssl.truststore.password", trustStorePassword);
+            config.put("ssl.keystore.type", "PKCS12");
+            config.put("ssl.keystore.location", keyStorePath);
+            config.put("ssl.keystore.password", keyStorePassword);
+            config.put("ssl.key.password", keyStorePassword);
+            config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            config.put("value.deserializer", "io.vertx.kafka.client.serialization.JsonObjectDeserializer");
+            config.put("group.id", "my_group");
+            config.put("auto.offset.reset", "latest");
+            config.put("enable.auto.commit", "false");
+
+
+            // Use consumer for interacting with Apache Kafka
+            KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, config);
+            consumer.handler(record -> {
+                LogController logController = new LogController();
+                logController.sendLog(record.key(), record.value());
+                System.out.println("Key: " + record.key() + " Value: " + record.value());
+            });
+
+            consumer.subscribe("Database", ar -> {
+                if(ar.succeeded())
+                    System.out.println("Successful Subscribe.");
+                else
+                    System.out.println("Failed Subscribe.");
+            });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
         hs.requestHandler(router)
                 .listen(8083);
